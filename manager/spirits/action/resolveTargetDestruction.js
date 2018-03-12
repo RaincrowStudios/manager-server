23b4df10e15e8bb40608072edc31168ecd824ef9
JiveSnake <jivetalkingsnakeman@gmail.com>
2018-03-12 12:25:51 -0400

    Adding Target destruction functionality

--- manager/spirits/action/resolveTargetDestruction.js
+++ manager/spirits/action/resolveTargetDestruction.js
@@ -2,6 +2,14 @@ const spiritDeath = require('../spiritDeath')
 const portalDestroy = require('../../portals/portalDestroy')
 
 module.exports = (targetInstance, target, instance) => {
-  spiritDeath(targetInstance, target, instance)
+  if (target.type === 'spirit') {
+    spiritDeath(targetInstance, target, instance)
+  }
+  else if (target.type === 'portal') {
+
+  }
+  else {
+    
+  }
   //portalDestroy(targetInstance, target, instance)
 }
