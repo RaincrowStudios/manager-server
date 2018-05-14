65300a97c23e7a29e8a5d26230ae89ea8f10f48d
Sean Landis <sean.a.landis@gmail.com>
2018-05-14 11:57:14 -0400

    Minor fix

--- lua/addExperience.lua
+++ lua/addExperience.lua
@@ -4,8 +4,8 @@ local xpGain = ARGV[1]
 
 local thresholds = cjson.decode(redis.call('HGET', 'list:constants', 'levelThresholds'))
 local baseEnergyByLevel = cjson.decode(redis.call('HGET', 'list:constants', 'baseEnergyByLevel'))
-local newXp = redis.call('HINCRBY', key, 'xp', xpGain)
-local nextLevel = cjson.decode(redis.call('HGET', key, 'level')) + 1
+local newXp = redis.call('HINCRBY', instance, 'xp', xpGain)
+local nextLevel = cjson.decode(redis.call('HGET', instance, 'level')) + 1
 
 if newXp >= thresholds[nextLevel] then
   redis.call('HMSET', instance, 'level', cjson.encode(nextLevel), 'baseEnergy', cjson.encode(baseEnergyByLevel[nextLevel]))
