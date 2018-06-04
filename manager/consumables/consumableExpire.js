900bcb57154e5c442ae07f1970b379e731639e0a
Keith Waters <keith@readingwaters.com>
2018-06-04 18:06:56 -0400

    Add consumables functions.

---
+++ manager/consumables/consumableExpire.js
@@ -0,0 +1,49 @@
+const getFieldsFromHash = require('../../redis/getFieldsFromHash')
+const getOneFromHash = require('../../redis/getOneFromHash')
+const incrementHashField = require('../../redis/incrementHashField')
+const removeFromActiveSet = require('../../redis/removeFromActiveSet')
+const removeFromList = require('../../redis/removeFromList')
+const updateHashFieldArray = require('../../redis/updateHashFieldArray')
+const informPlayers = require('../../utils/informPlayers')
+
+module.exports = async (consumableInstance) => {
+  try {
+    const consumable = await getOneFromHash('list:active:consumables', consumableInstance)
+console.log(consumable)
+    if (consumable) {
+      const [player, activeConsumables] =
+        await getFieldsFromHash(consumable.bearer, ['player', 'activeConsumables'])
+
+      const index = activeConsumables 
+        .map(oldConsumable => oldConsumable.id)
+        .indexOf(consumable.id)
+
+      await Promise.all([
+        informPlayers(
+          [player],
+          {
+            command: 'character_consumable_remove',
+            instance: consumableInstance,
+          }
+        ),
+        removeFromActiveSet('consumables', consumableInstance),
+        removeFromList('consumables', consumableInstance),
+        incrementHashField(
+            consumable.bearer,
+            consumable.effectedStat,
+            consumable.effectedAmount*-1
+        ), 
+        updateHashFieldArray(
+          consumable.bearer,
+          'remove',
+          'activeConsumables',
+          consumable,
+          index
+        )
+      ])
+    }
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
