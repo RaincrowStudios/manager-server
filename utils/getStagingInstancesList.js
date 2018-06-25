5c42c33b1c6285efb152dd410e144e3c23ca1c39
Sean Landis <sean.a.landis@gmail.com>
2018-06-25 12:18:13 -0400

    Fixing initializer

--- utils/getStagingInstancesList.js
+++ utils/getStagingInstancesList.js
@@ -1,39 +1,6 @@
 const { google } = require('googleapis')
 const compute = google.compute('beta')
 
-module.exports = authorize(function(authClient) {
-  const request = {
-    project: 'raincrow-pantheon',
-    zone: process.env.INSTANCE_REGION.split('/').pop(),
-    instanceGroup: 'staging-manager-group',
-    resource: {},
-    auth: authClient,
-  }
-
-  const handlePage = function(err, response) {
-    if (err) {
-      console.error(err)
-      return
-    }
-
-    const itemsPage = response['items']
-    if (!itemsPage) {
-      return
-    }
-    for (let i = 0; i < itemsPage.length; i++) {
-      //Change code below to process each resource in `itemsPage`:
-      console.log(JSON.stringify(itemsPage[i], null, 2))
-    }
-
-    if (response.nextPageToken) {
-      request.pageToken = response.nextPageToken
-      compute.instanceGroups.listInstances(request, handlePage)
-    }
-  }
-
-  compute.instanceGroups.listInstances(request, handlePage)
-})
-
 function authorize(callback) {
   google.auth.getApplicationDefault(function(err, authClient) {
     if (err) {
@@ -45,5 +12,49 @@ function authorize(callback) {
       authClient = authClient.createScoped(scopes)
     }
     callback(authClient)
+  })
+}
+
+module.exports = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      authorize(function(authClient) {
+        const request = {
+          project: 'raincrow-pantheon',
+          zone: process.env.INSTANCE_REGION.split('/').pop(),
+          instanceGroup: 'staging-manager-group',
+          resource: {},
+          auth: authClient,
+        }
+
+        const handlePage = function(err, response) {
+          if (err) {
+            console.error(err)
+            throw new Error(err)
+          }
+
+          const itemsPage = response['items']
+          if (!itemsPage) {
+            resolve(false)
+          }
+          for (let i = 0; i < itemsPage.length; i++) {
+            //Change code below to process each resource in `itemsPage`:
+            console.log(JSON.stringify(itemsPage[i], null, 2))
+          }
+
+          if (response.nextPageToken) {
+            request.pageToken = response.nextPageToken
+            compute.instanceGroups.listInstances(request, handlePage)
+          }
+        }
+
+        compute.instanceGroups.listInstances(request, handlePage)
+      })
+
+      resolve(true)
+    }
+    catch (err) {
+      reject(err)
+    }
   })
 }
