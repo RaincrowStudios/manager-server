65d4a964eaaa917340f0ac2bd3fcf2df6f09aad0
Sean Landis <sean.a.landis@gmail.com>
2018-09-04 11:16:09 -0400

    Fixed map token creation to match game-server

--- utils/createMapToken.js
+++ utils/createMapToken.js
@@ -1,9 +1,20 @@
 module.exports = (info) => {
   let token
   switch (info.type) {
+    case 'location':
+      token = {
+        instance: info.instance,
+        type: info.type,
+        tier: info.tier,
+        latitude: info.latitude,
+        longitude: info.longitude,
+        physicalOnly: info.physicalOnly
+      }
+      break
     case 'portal':
       token = {
         instance: info.instance,
+        creator: info.ownerDisplay,
         type: info.type,
         degree: info.degree,
         latitude: info.latitude,
@@ -24,22 +35,28 @@ module.exports = (info) => {
     case 'spirit':
       token = {
         instance: info.instance,
+        coven: info.coven || '',
         type: info.type,
         degree: info.degree,
-        latitude: info.latitude,
-        longitude: info.longitude
+        latitude: info.latitude || 0,
+        longitude: info.longitude || 0,
+        position: info.position || 0,
       }
       break
     case 'witch':
       token = {
         instance: info.instance,
         displayName: info.displayName,
+        coven: info.coven || '',
+        state: info.state,
         type: info.type,
         male: info.male,
         degree:  info.degree,
         latitude: info.fuzzyLatitude,
         longitude: info.fuzzyLongitude,
-        distance: info.distance
+        position: info.position || 0,
+        physical: info.distance ? false : true,
+        immunityList: Object.values(info.immunities).map(immunity => immunity.caster)
       }
       break
     default:
