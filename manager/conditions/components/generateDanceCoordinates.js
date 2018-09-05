86c9dd92b26a072200dd632f272158f801d9e195
Sean Landis <sean.a.landis@gmail.com>
2018-09-05 19:09:22 -0400

    Adding Duke functionality

---
+++ manager/conditions/components/generateDanceCoordinates.js
@@ -0,0 +1,19 @@
+const checkGeohashOutOfRange = require('./checkGeohashOutOfRange')
+
+module.exports = (latitude, longitude) => {
+  let newLatitude =
+    latitude + (((Math.floor(Math.random() * (100 - 50)) + 50) * 0.00001) *
+    (Math.random() < 0.5 ? -1 : 1))
+
+  let newLongitude =
+    longitude + (((Math.floor(Math.random() * (100 - 50)) + 50) * 0.00001 *
+    Math.cos(latitude * (Math.PI / 180)))  *
+    (Math.random() < 0.5 ? -1 : 1))
+
+  if(checkGeohashOutOfRange(newLatitude, newLongitude)) {
+    newLatitude = newLatitude + (Math.sign(newLatitude) * -0.01)
+    newLongitude = newLongitude + (Math.sign(newLongitude) * -0.01)
+  }
+
+  return [newLatitude, newLongitude]
+}
