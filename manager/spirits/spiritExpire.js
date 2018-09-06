a6f9e4eaaf70c65e20dbc2b1feb59c11b43c58e2
Sean Landis <sean.a.landis@gmail.com>
2018-09-06 14:00:56 -0400

    turned into promise for use in Location Reward

--- manager/spirits/spiritExpire.js
+++ manager/spirits/spiritExpire.js
@@ -2,89 +2,94 @@ const timers = require('../../database/timers')
 const getAllFromHash = require('../../redis/getAllFromHash')
 const removeFromAll = require('../../redis/removeFromAll')
 const updateHashFieldObject = require('../../redis/updateHashFieldObject')
+const handleError = require('../../utils/handleError')
 const informNearbyPlayers = require('../../utils/informNearbyPlayers')
 const informPlayers = require('../../utils/informPlayers')
 const deleteAllConditions = require('../conditions/deleteAllConditions')
 
-module.exports = async (spiritInstance) => {
-  try {
-    const instanceInfo = await getAllFromHash(spiritInstance)
+module.exports = (spiritInstance) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const instanceInfo = await getAllFromHash(spiritInstance)
 
-    if (instanceInfo) {
-      const update = []
-      const inform = []
+      if (instanceInfo) {
+        const update = []
+        const inform = []
 
-      update.push(
-        removeFromAll('spirits', spiritInstance)
-      )
-
-      if (instanceInfo.location) {
         update.push(
-          updateHashFieldObject(
-            instanceInfo.location,
-            'remove',
-            'spirits',
-            spiritInstance
-          )
+          removeFromAll('spirits', spiritInstance)
         )
-      }
 
-      if (instanceInfo.conditions) {
-        update.push(deleteAllConditions(instanceInfo.conditions))
-      }
-
-      if (instanceInfo.owner) {
-        update.push[
-          informPlayers(
-            [instanceInfo.player],
-            {
-              command: 'character_spirit_expire',
-              spirit: spiritInstance,
-              displayName: instanceInfo.displayName
-            }
-          ),
-          updateHashFieldObject(
-            instanceInfo.owner,
-            'remove',
-            'activeSpirits',
-            spiritInstance
+        if (instanceInfo.location) {
+          update.push(
+            updateHashFieldObject(
+              instanceInfo.location,
+              'remove',
+              'spirits',
+              spiritInstance
+            )
           )
-        ]
-      }
+        }
 
-      inform.push(
-        {
-          function: informNearbyPlayers,
-          parameters: [
-            instanceInfo,
-            {
-              command: 'map_token_remove',
-              instance: spiritInstance
-            },
-            Object.values(instanceInfo.conditions)
-              .filter(condition => condition.status === 'invisible').length ?
-              1 : 0
+        if (instanceInfo.conditions) {
+          update.push(deleteAllConditions(instanceInfo.conditions))
+        }
+
+        if (instanceInfo.owner) {
+          update.push[
+            informPlayers(
+              [instanceInfo.player],
+              {
+                command: 'character_spirit_expire',
+                spirit: spiritInstance,
+                displayName: instanceInfo.displayName
+              }
+            ),
+            updateHashFieldObject(
+              instanceInfo.owner,
+              'remove',
+              'activeSpirits',
+              spiritInstance
+            )
           ]
         }
-      )
 
-      await Promise.all(update)
+        inform.push(
+          {
+            function: informNearbyPlayers,
+            parameters: [
+              instanceInfo,
+              {
+                command: 'map_token_remove',
+                instance: spiritInstance
+              },
+              Object.values(instanceInfo.conditions)
+                .filter(condition => condition.status === 'invisible').length ?
+                1 : 0
+            ]
+          }
+        )
 
-      for (const informObject of inform) {
-        const informFunction = informObject.function
-        await informFunction(...informObject.parameters)
+        await Promise.all(update)
+
+        for (const informObject of inform) {
+          const informFunction = informObject.function
+          await informFunction(...informObject.parameters)
+        }
       }
-    }
 
-    const spiritTimers = timers.by('instance', spiritInstance)
-    if (spiritTimers) {
-      clearTimeout(spiritTimers.expireTimer)
-      clearTimeout(spiritTimers.moveTimer)
-      clearTimeout(spiritTimers.actionTimer)
-      timers.remove(spiritTimers)
+      const spiritTimers = timers.by('instance', spiritInstance)
+      if (spiritTimers) {
+        clearTimeout(spiritTimers.expireTimer)
+        clearTimeout(spiritTimers.moveTimer)
+        clearTimeout(spiritTimers.actionTimer)
+        timers.remove(spiritTimers)
+      }
+
+      resolve(true)
     }
-  }
-  catch (err) {
-    console.error(err)
-  }
+    catch (err) {
+      reject(handleError(err))
+    }
+  })
 }
