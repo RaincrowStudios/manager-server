951d6bce495512718400d7a94d7ddc93e93a3f33
Sean Landis <sean.a.landis@gmail.com>
2018-09-10 14:00:59 -0400

    Fixed coord generation

--- utils/generateNewCoordinates.js
+++ utils/generateNewCoordinates.js
@@ -1,30 +1,40 @@
-const checkGeohashOutOfRange = require('./checkGeohashOutOfRange')
+function degreeToRadians(degree) {
+  return degree * (Math.PI/180)
+}
+
+function radiansToDegrees(radian) {
+  return radian * (180/Math.PI)
+}
 
 function precisionRound(number, precision) {
   const factor = Math.pow(10, precision)
   return Math.round(number * factor) / factor
 }
 
-module.exports = (latitude, longitude, min, max) => {
-  let newLatitude =
-    latitude + (((Math.floor(Math.random() * (max - min)) + min) * 0.00001) *
-    (Math.random() < 0.5 ? -1 : 1))
+module.exports = (latitudeDegree, longitudeDegree, min, max, direction = false) => {
+  const earthRadius = 6371
+  const distance = Math.floor(Math.random() * (max/1000 - min/1000)) + min/1000
+  const bearing = direction !== false ? direction : Math.floor(Math.random() * 360)
+  const latitude = degreeToRadians(latitudeDegree)
+  const longitude = degreeToRadians(longitudeDegree)
 
-  let newLongitude =
-    longitude + (((Math.floor(Math.random() * (max - min)) + min) * 0.00001 *
-    Math.cos(latitude * (Math.PI / 180)))  *
-    (Math.random() < 0.5 ? -1 : 1))
+  let newLatitude = Math.asin(
+    Math.sin(latitude) * Math.cos(distance/earthRadius) +
+    Math.cos(latitude) * Math.sin(distance/earthRadius) * Math.cos(bearing)
+  )
 
-  if (checkGeohashOutOfRange(newLatitude, newLongitude)) {
-    newLatitude = newLatitude + (Math.sign(newLatitude) * -0.01)
-    newLongitude = newLongitude + (Math.sign(newLongitude) * -0.01)
+  let newLongitude = longitude + Math.atan2(
+    Math.sin(bearing) * Math.sin(distance/earthRadius) * Math.cos(latitude),
+    Math.cos(distance/earthRadius) - Math.sin(latitude) * Math.sin(newLatitude))
+
+  newLongitude = (newLongitude + 540) % 360 - 180
+
+  newLatitude = radiansToDegrees(newLatitude)
+  newLongitude = radiansToDegrees(newLongitude)
+
+  if (newLatitude < -85.05112878 || newLatitude > 85.05112878) {
+    newLatitude = (Math.sign(newLatitude) * 85.05)
   }
 
-  const newCoords = [
-    precisionRound(newLatitude, 6),
-    precisionRound(newLongitude, 6)
-  ]
-
-
-  return newCoords
+  return [precisionRound(newLatitude, 6), precisionRound(newLongitude, 6)]
 }
