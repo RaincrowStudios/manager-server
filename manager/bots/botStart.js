2197b2219075a0691d4bab215fa0637f31514ddc
Sean Landis <sean.a.landis@gmail.com>
2018-10-31 16:41:53 -0400

    Added start functionality

---
+++ manager/bots/botStart.js
@@ -0,0 +1,41 @@
+const timers = require('../../database/timers')
+const getFieldsFromHash = require('../../redis/getFieldsFromHash')
+const handleError = require('../../utils/handleError')
+const botAction = require('./botAction')
+const botMove = require('./botMove')
+
+module.exports = async (botInstance) => {
+  try {
+    const botTimers = timers.by('instance', botInstance)
+
+    const {actionOn, moveOn} = await getFieldsFromHash(
+      botInstance,
+      ['actionOn', 'moveOn']
+    )
+
+    const currentTime = Date.now()
+
+    const actionTimer =
+      setTimeout(() =>
+        botAction(botInstance),
+        actionOn - currentTime
+      )
+
+    botTimers.actionTimer = actionTimer
+
+    const moveTimer =
+      setTimeout(() =>
+        botMove(botInstance),
+        moveOn - currentTime
+      )
+
+    botTimers.moveTimer = moveTimer
+
+    timers.insert(botTimers)
+
+    return true
+  }
+  catch (err) {
+    return handleError(err)
+  }
+}
---
+++ manager/spirits/spiritStart.js
@@ -0,0 +1,43 @@
+const timers = require('../../database/timers')
+const getFieldsFromHash = require('../../redis/getFieldsFromHash')
+const handleError = require('../../utils/handleError')
+const spiritMove = require('./spiritMove')
+const spiritAction = require('./spiritAction')
+
+module.exports = async (spiritInstance) => {
+  try {
+    const spiritTimers = timers.by('instance', spiritInstance)
+
+    const {actionOn, moveOn} = await getFieldsFromHash(
+      spiritInstance,
+      ['actionOn', 'moveOn']
+    )
+
+    const currentTime = Date.now()
+
+    const actionTimer =
+      setTimeout(() =>
+        spiritAction(spiritInstance),
+        actionOn - currentTime
+      )
+
+    spiritTimers.actionTimer = actionTimer
+
+    if (moveOn) {
+      const moveTimer =
+        setTimeout(() =>
+          spiritMove(spiritInstance),
+          moveOn - currentTime
+        )
+
+      spiritTimers.moveTimer = moveTimer
+    }
+
+    timers.insert(spiritTimers)
+
+    return true
+  }
+  catch (err) {
+    return handleError(err)
+  }
+}
