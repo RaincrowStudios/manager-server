979c38beaeea96d902269f33da102b4d81c85914
Sean Landis <sean.a.landis@gmail.com>
2018-11-06 16:57:33 -0500

    Updated dependencies

--- utils/stopTimers.js
+++ utils/stopTimers.js
@@ -1,6 +1,6 @@
 const timers = require('../database/timers')
 
-module.exports = (instance) => {
+module.exports = instance => {
   const timersToClear = timers.by('instance', instance)
 
   if (timersToClear) {
