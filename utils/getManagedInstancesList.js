a9f07a5e536a2d5778de8f9ec2e6fecafb5487bc
Sean Landis <sean.a.landis@gmail.com>
2018-11-26 12:19:03 -0500

    Formatting

--- utils/getManagedInstancesList.js
+++ utils/getManagedInstancesList.js
@@ -1,52 +1,52 @@
-const { google } = require("googleapis");
-const compute = google.compute("beta");
+const { google } = require('googleapis')
+const compute = google.compute('beta')
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
     try {
       google.auth.getApplicationDefault((err, authClient) => {
         if (err) {
-          throw new Error(err);
+          throw new Error(err)
         }
         if (
           authClient.createScopedRequired &&
           authClient.createScopedRequired()
         ) {
-          const scopes = ["https://www.googleapis.com/auth/cloud-platform"];
-          authClient = authClient.createScoped(scopes);
+          const scopes = ['https://www.googleapis.com/auth/cloud-platform']
+          authClient = authClient.createScoped(scopes)
         }
 
         const request = {
-          project: "raincrow-pantheon",
-          region: process.env.INSTANCE_REGION.split("/")
+          project: 'raincrow-pantheon',
+          region: process.env.INSTANCE_REGION.split('/')
             .pop()
             .slice(0, -2),
           instanceGroupManager:
             process.env.NODE_ENV +
-            "-manager-group-" +
-            process.env.INSTANCE_REGION.split("/")
+            '-manager-group-' +
+            process.env.INSTANCE_REGION.split('/')
               .pop()
               .slice(0, -2),
           auth: authClient
-        };
+        }
 
         compute.regionInstanceGroupManagers.listManagedInstances(
           request,
           (err, response) => {
             if (err) {
-              throw new Error(err);
+              throw new Error(err)
             } else {
               const managedInstanceIds = response.data.managedInstances.map(
                 instance => instance.id
-              );
+              )
 
-              resolve(managedInstanceIds);
+              resolve(managedInstanceIds)
             }
           }
-        );
-      });
+        )
+      })
     } catch (err) {
-      reject(err);
+      reject(err)
     }
-  });
-};
+  })
+}
