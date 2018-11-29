a11f71e9226b3dcc96326e46ea3aaa8b4797c190
Sean Landis <sean.a.landis@gmail.com>
2018-11-29 13:08:19 -0500

    Checking that bots are stopping properly

--- manager/bots/botAction.js
+++ manager/bots/botAction.js
@@ -1,55 +1,56 @@
-const timers = require("../../database/timers");
-const getFieldsFromHash = require("../../redis/getFieldsFromHash");
-const updateHashField = require("../../redis/updateHashField");
-const handleError = require("../../utils/handleError");
-const informGame = require("../../utils/informGame");
+const timers = require('../../database/timers')
+const getFieldsFromHash = require('../../redis/getFieldsFromHash')
+const updateHashField = require('../../redis/updateHashField')
+const handleError = require('../../utils/handleError')
+const informGame = require('../../utils/informGame')
 
 async function botAction(botInstance) {
   try {
     const { state, actionFreq } = await getFieldsFromHash(botInstance, [
-      "state",
-      "actionFreq"
-    ]);
+      'state',
+      'actionFreq'
+    ])
 
     if (actionFreq) {
-      if (state !== "dead") {
-        informGame(botInstance, "covens", "head", "covens/npe/action");
+      if (state !== 'dead') {
+        console.log('bot acting...')
+        informGame(botInstance, 'covens', 'head', 'covens/npe/action')
       }
 
-      const currentTime = Date.now();
+      const currentTime = Date.now()
 
-      let newActionOn, seconds;
-      if (actionFreq.includes("-")) {
-        const [min, max] = actionFreq.split("-");
+      let newActionOn, seconds
+      if (actionFreq.includes('-')) {
+        const [min, max] = actionFreq.split('-')
 
         seconds =
           Math.floor(
             Math.random() * (parseInt(max, 10) - parseInt(min, 10) + 1)
-          ) + parseInt(min, 10);
+          ) + parseInt(min, 10)
       } else {
-        seconds = parseInt(actionFreq, 10);
+        seconds = parseInt(actionFreq, 10)
       }
 
-      newActionOn = currentTime + seconds * 1000;
+      newActionOn = currentTime + seconds * 1000
 
-      await updateHashField(botInstance, "actionOn", newActionOn);
+      await updateHashField(botInstance, 'actionOn', newActionOn)
 
       const newTimer = setTimeout(
         () => botAction(botInstance),
         newActionOn - currentTime
-      );
+      )
 
-      const botTimers = timers.by("instance", botInstance);
+      const botTimers = timers.by('instance', botInstance)
       if (botTimers) {
-        botTimers.actionTimer = newTimer;
-        timers.update(botTimers);
+        botTimers.actionTimer = newTimer
+        timers.update(botTimers)
       }
     }
 
-    return true;
+    return true
   } catch (err) {
-    return handleError(err);
+    return handleError(err)
   }
 }
 
-module.exports = botAction;
+module.exports = botAction
--- manager/bots/botMove.js
+++ manager/bots/botMove.js
@@ -1,55 +1,56 @@
-const timers = require("../../database/timers");
-const getFieldsFromHash = require("../../redis/getFieldsFromHash");
-const updateHashField = require("../../redis/updateHashField");
-const handleError = require("../../utils/handleError");
-const informGame = require("../../utils/informGame");
+const timers = require('../../database/timers')
+const getFieldsFromHash = require('../../redis/getFieldsFromHash')
+const updateHashField = require('../../redis/updateHashField')
+const handleError = require('../../utils/handleError')
+const informGame = require('../../utils/informGame')
 
 async function botMove(botInstance) {
   try {
     const { state, moveFreq } = await getFieldsFromHash(botInstance, [
-      "state",
-      "moveFreq"
-    ]);
+      'state',
+      'moveFreq'
+    ])
 
     if (moveFreq) {
-      if (state !== "dead") {
-        informGame(botInstance, "covens", "head", "covens/npe/move");
+      if (state !== 'dead') {
+        console.log('bot moving...')
+        informGame(botInstance, 'covens', 'head', 'covens/npe/move')
       }
 
-      const currentTime = Date.now();
+      const currentTime = Date.now()
 
-      let newMoveOn, seconds;
-      if (moveFreq.includes("-")) {
-        const [min, max] = moveFreq.split("-");
+      let newMoveOn, seconds
+      if (moveFreq.includes('-')) {
+        const [min, max] = moveFreq.split('-')
 
         seconds =
           Math.floor(
             Math.random() * (parseInt(max, 10) - parseInt(min, 10) + 1)
-          ) + parseInt(min, 10);
+          ) + parseInt(min, 10)
       } else {
-        seconds = parseInt(moveFreq, 10);
+        seconds = parseInt(moveFreq, 10)
       }
 
-      newMoveOn = currentTime + seconds * 1000;
+      newMoveOn = currentTime + seconds * 1000
 
-      await updateHashField(botInstance, "moveOn", newMoveOn);
+      await updateHashField(botInstance, 'moveOn', newMoveOn)
 
       const newTimer = setTimeout(
         () => botMove(botInstance),
         newMoveOn - currentTime
-      );
+      )
 
-      const botTimers = timers.by("instance", botInstance);
+      const botTimers = timers.by('instance', botInstance)
       if (botTimers) {
-        botTimers.moveTimer = newTimer;
-        timers.update(botTimers);
+        botTimers.moveTimer = newTimer
+        timers.update(botTimers)
       }
     }
 
-    return true;
+    return true
   } catch (err) {
-    return handleError(err);
+    return handleError(err)
   }
 }
 
-module.exports = botMove;
+module.exports = botMove
